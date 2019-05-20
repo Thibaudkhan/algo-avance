dcaa09f6eb8480b05f0c8b3775b1e39648182903
Unknown <thibaud.roussel@edu.itescia.fr>
2019-05-20 11:17:02 +0200

    Mon code !!!!!
    Thibaud

--- exo/exercice1/figoThibaud.php
+++ exo/exercice1/figoThibaud.php
@@ -1,22 +1,19 @@
 <?php
 
-<<<<<<< HEAD:exo/exercice1.php
-$n = 10;
-=======
 $n = 0;
->>>>>>> master:exo/exercice1/figoThibaud.php
 
 function fibo($n){
 
-    $u0 = 0;
-    $u1 = 1;
+	$u = 0;
+	$u1 = 1;
 
-    for ($i = 2; $i <= $n; $i ++) {
-        $result = $u0 + $u1;
-        $u0 = $u1;
-        $u1 = $result;
-    }
-    echo $result . PHP_EOL;
+	while ($n < 28) {
+		$result = $u + $u1 ;
+		$u = $u1;
+		$u1 = $result;
+		$n++;	
+	}
+	echo $result."\n";
 }
 
 fibo($n);
