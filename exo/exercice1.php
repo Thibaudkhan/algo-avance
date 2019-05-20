bdd3a295f43c4abda1dbabb6d96f7cf6e06d499d
Unknown <thibaud.roussel@edu.itescia.fr>
2019-05-20 10:47:33 +0200

    Suite fibo

--- exo/exercice1.php
+++ exo/exercice1.php
@@ -1,5 +1,21 @@
 <?php
 
-$test = "Je suis un test \n";
 
-echo $test;
+$n = 0;
+
+
+function fibo($n){
+
+	$u = 0;
+	$u1 = 1;
+
+	while ($n < 28) {
+		echo $result."\n";
+		$result = $u + $u1 ;
+		$u = $u1;
+		$u1 = $result;
+		$n++;	
+	}
+}
+
+fibo($n);
