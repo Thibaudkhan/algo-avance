9f028e333a77e684f9968025636ae2f0b8f19ea7
Unknown <kiwol95@gmail.com>
2019-05-21 11:39:54 +0200

    MOB BUBBLE SORT (+ ADVANCED)

--- exo/exercice3/mob.php
+++ exo/exercice3/mob.php
@@ -15,13 +15,41 @@ function randomArray(int $nbInt):array{
 }
 
 function bubbleSort($arrayInt):array{
-    
+    do {
+        $nbChange = 0;
+        for ($i = 0; $i < count($arrayInt) - 1; $i++) {
+            if ($arrayInt[$i] > $arrayInt[$i + 1]) {
+                $temp = $arrayInt[$i + 1];
+                $arrayInt[$i + 1] = $arrayInt[$i];
+                $arrayInt[$i] = $temp;
+                $nbChange ++;
+            }
+        }
+    } while ($nbChange != 0);
+    return $arrayInt;
+}
+
+function bubbleSortAd($arrayInt):array{
+    $arrayLength = count($arrayInt);
+    do {
+        $nbChange = 0;
+        for ($i = 0; $i < $arrayLength - 1; $i++) {
+            if ($arrayInt[$i] > $arrayInt[$i + 1]) {
+                $temp = $arrayInt[$i + 1];
+                $arrayInt[$i + 1] = $arrayInt[$i];
+                $arrayInt[$i] = $temp;
+                $nbChange ++;
+            }
+        }
+        $arrayLength -= 1;
+    } while ($nbChange != 0);
+    return $arrayInt;
 }
 
 $arrayOriginal = randomArray(10);
-$arrayCopy = $arrayOriginal;
-sort($arrayCopy);
+// $arrayCopy = $arrayOriginal;
+// sort($arrayCopy);
 print_r($arrayOriginal);
-print_r($arrayCopy);
+print_r(bubbleSortAd($arrayOriginal));
 
 ?>
