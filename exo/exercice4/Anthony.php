5815fd85c70ca07583c02937dfd9ca84e4005592
Unknown <anthony.guiock@edu.itescia.fr>
2019-05-22 10:00:22 +0200

    Exo4 Anthony

---
+++ exo/exercice4/Anthony.php
@@ -0,0 +1,19 @@
+<?php
+function getNbDigit(int $nbToCut):int {
+	if($nbToCut < 10){
+		return 1;
+	} else{
+		return 1 + getNbDigit(intdiv($nbToCut, 10));
+	}
+}
+
+function powRec(int $nbToPow, int $pow):int{
+	if($pow == 1){
+		return $nbToPow;
+	}else{
+		return $nbToPow * powRec($nbToPow, $pow - 1);
+	}
+}
+
+echo powRec(3,3);
+  ?>
