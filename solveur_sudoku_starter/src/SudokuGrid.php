3a9acd0a07046a4560884bc1aaa44ac40d31e0f7
Unknown <anthony.guiock@edu.itescia.fr>
2019-05-23 12:01:07 +0200

    Lecture Json + split Array

--- solveur_sudoku_starter/src/SudokuGrid.php
+++ solveur_sudoku_starter/src/SudokuGrid.php
@@ -1,6 +1,21 @@
 <?php
 
-class SudokuGrid implements GridInterface
+class SudokuGrid //implements GridInterface
 {
-    /* Insérer le code ici */
+
 }
+function jsonToArrays($filepath) : array{
+	$stream = fopen($filepath, "r");
+	$jsonString = fread($stream, filesize($filepath));
+	$jsonString = json_decode($jsonString);
+	return $jsonString;
+}
+
+function arrayToArrays(array $bigArray) : array{
+	return array_chunk($bigArray, 3);
+}
+
+//Test lecture Json + découpage tableau (Anthony)
+foreach (jsonToArrays("../grids/full.json") as $array) {
+ 	print_r(arrayToArrays($array));
+ } 
